cb4c1111f5bc0f46e33e2723376a72386c1c0a1a
Adhocmaster <adhocmaster@live.com>
2016-05-16 03:23:05 +0600

    tmp test directory added

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,16 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = 'G:\wamp\www\wp452';
+	
+	if(file_exists('/tmp/wordpress-tests-lib')) {
+		
+		$_tests_dir = '/tmp/wordpress-tests-lib';
+
+	} else {
+
+		$_tests_dir = 'G:\wamp\www\wp452';
+
+	}
 }
 
 // Give access to tests_add_filter() function.
